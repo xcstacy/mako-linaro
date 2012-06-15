a5ed11a80e3c404f13e003c0a8f5ab7456a5bc6d
Gokhan Moral <gm@alumni.bilkent.edu.tr>
2012-06-16 00:01:43 +0300

    mmc: disable MMC_CAP_ERASE for MACH_U1 in case I try to build this repo for s2
    and build script + tiny fix in tmu for s2

--- arch/arm/mach-exynos/tmu.c
+++ arch/arm/mach-exynos/tmu.c
@@ -73,6 +73,9 @@ extern int mali_voltage_lock_init(void);
 extern int mali_voltage_lock_push(int lock_vol);
 extern int mali_voltage_lock_pop(void);
 #define CONFIG_TC_VOLTAGE /* Temperature compensated voltage */
+#else
+#define mali_voltage_lock_push(msg...) 0
+#define mali_voltage_lock_pop(msg...) 0
 #endif
 
 static unsigned int get_curr_temp(struct s5p_tmu_info *info)
