390a15868c6e90971e5ea43cf03b0b4161310a88
Gokhan Moral <gm@alumni.bilkent.edu.tr>
2012-07-01 02:01:43 +0300

    cpufreq_pegasusq: a bugfix and a little tweaking

--- drivers/cpufreq/cpufreq_pegasusq.c
+++ drivers/cpufreq/cpufreq_pegasusq.c
@@ -168,12 +168,12 @@ static unsigned int get_nr_run_avg(void)
 
 #ifdef CONFIG_MACH_MIDAS
 static int hotplug_rq[4][2] = {
-	{0, 150}, {150, 300}, {300, 400}, {400, 0}
+	{0, 100}, {100, 300}, {300, 400}, {400, 0}
 };
 
 static int hotplug_freq[4][2] = {
 	{0, 500000},
-	{200000, 500000},
+	{400000, 500000},
 	{400000, 800000},
 	{500000, 0}
 };
@@ -960,7 +960,7 @@ static int check_down(void)
 		&& online > dbs_tuners_ins.max_cpu_lock)
 		return 1;
 
-	if (num_hist == 0 || num_hist % down_rate)
+	if (num_hist % down_rate)
 		return 0;
     if(num_hist == 0) num_hist = MAX_HOTPLUG_RATE; //make it circular -gm
 
