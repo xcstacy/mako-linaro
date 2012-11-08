CyanogenMod/android_kernel_samsung_smdk4412

mdnie: cyanogenmod

drivers/video/samsung/mdnie_table_c1m0.h
@@ -4,6 +4,26 @@
 #include "mdnie.h"
 
 
+static const unsigned short tune_cyanogenmod[] = {
+	0x0000, 0x0000, /*BANK 0*/
+	0x0008, 0x000c, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
+	0x0030, 0x0000, /*FA cs1 de8 hdr2 fa1*/
+	0x0092, 0x0040, /*DE pe*/
+	0x0093, 0x0040, /*DE pf*/
+	0x0094, 0x0040, /*DE pb*/
+	0x0095, 0x0040, /*DE ne*/
+	0x0096, 0x0040, /*DE nf*/
+	0x0097, 0x0040, /*DE nb*/
+	0x0098, 0x1000, /*DE max ratio*/
+	0x0099, 0x0100, /*DE min ratio*/
+	0x00b0, 0x1010, /*CS hg ry*/
+	0x00b1, 0x1010, /*CS hg gc*/
+	0x00b2, 0x1010, /*CS hg bm*/
+	0x00b3, 0x1204, /*CS weight grayTH*/
+	0x00ff, 0x0000, /*Mask Release*/
+	END_SEQ, 0x0000,
+};
+
 static const unsigned short tune_dynamic_gallery[] = {
 	0x0000, 0x0000, /*BANK 0*/
 	0x0008, 0x008c, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
@@ -733,6 +753,7 @@ struct mdnie_tunning_info etc_table[CABC_MAX][OUTDOOR_MAX][TONE_MAX] = {
 struct mdnie_tunning_info tunning_table[CABC_MAX][MODE_MAX][SCENARIO_MAX] = {
 	{
 		{
+            {"CYANOGENMOD",			tune_cyanogenmod},
 			{"DYNAMIC_UI",			tune_dynamic_ui},
 			{"DYNAMIC_VIDEO_NORMAL",	tune_dynamic_video},
 			{"DYNAMIC_VIDEO_WARM",		tune_dynamic_video},
@@ -742,6 +763,7 @@ struct mdnie_tunning_info tunning_table[CABC_MAX][MODE_MAX][SCENARIO_MAX] = {
 			{"DYNAMIC_GALLERY",		tune_dynamic_gallery},
 			{"DYNAMIC_VT",			tune_dynamic_vtcall},
 		}, {
+            {"CYANOGENMOD",			tune_cyanogenmod},
 			{"STANDARD_UI",			tune_standard_ui},
 			{"STANDARD_VIDEO_NORMAL",	tune_standard_video},
 			{"STANDARD_VIDEO_WARM",		tune_standard_video},
@@ -751,6 +773,7 @@ struct mdnie_tunning_info tunning_table[CABC_MAX][MODE_MAX][SCENARIO_MAX] = {
 			{"STANDARD_GALLERY",		tune_standard_gallery},
 			{"STANDARD_VT",			tune_standard_vtcall},
 		}, {
+            {"CYANOGENMOD",			tune_cyanogenmod},
 			{"NATURAL_UI",			tune_natural_ui},
 			{"NATURAL_VIDEO_NORMAL",	tune_natural_video},
 			{"NATURAL_VIDEO_WARM",		tune_natural_video},
@@ -760,6 +783,7 @@ struct mdnie_tunning_info tunning_table[CABC_MAX][MODE_MAX][SCENARIO_MAX] = {
 			{"NATURAL_GALLERY",		tune_natural_gallery},
 			{"NATURAL_VT",			tune_natural_vtcall},
 		}, {
+            {"CYANOGENMOD",			tune_cyanogenmod},
 			{"MOVIE_UI",			tune_movie_ui},
 			{"MOVIE_VIDEO_NORMAL",		tune_movie_video},
 			{"MOVIE_VIDEO_WARM",		tune_movie_video},

xcstacy/mako-linaro

Change-Id: Id51499b4d2b7bf149a835256f03de125fe39b23a
Conflicts:
	drivers/video/samsung/mdnie_table_c1m0.h

drivers/video/samsung/mdnie_table_c1m0.h
@@ -4,6 +4,26 @@
 #include "mdnie.h"
 
 
+static const unsigned short tune_cyanogenmod[] = {
+	0x0000, 0x0000, /*BANK 0*/
+	0x0008, 0x0088, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
+	0x0030, 0x0000, /*FA cs1 de8 hdr2 fa1*/
+	0x0092, 0x0040, /*DE pe*/
+	0x0093, 0x0040, /*DE pf*/
+	0x0094, 0x0040, /*DE pb*/
+	0x0095, 0x0040, /*DE ne*/
+	0x0096, 0x0040, /*DE nf*/
+	0x0097, 0x0040, /*DE nb*/
+	0x0098, 0x1000, /*DE max ratio*/
+	0x0099, 0x0100, /*DE min ratio*/
+	0x00b0, 0x1010, /*CS hg ry*/
+	0x00b1, 0x1010, /*CS hg gc*/
+	0x00b2, 0x1010, /*CS hg bm*/
+	0x00b3, 0x1404, /*CS weight grayTH*/
+	0x00ff, 0x0000, /*Mask Release*/
+	END_SEQ, 0x0000,
+};
+
 static const unsigned short tune_dynamic_gallery[] = {
 	0x0000, 0x0000, /*BANK 0*/
 	0x0008, 0x008c, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
@@ -733,6 +753,7 @@ static struct mdnie_tunning_info etc_table[CABC_MAX][OUTDOOR_MAX][TONE_MAX] = {
 static struct mdnie_tunning_info tunning_table[CABC_MAX][MODE_MAX][SCENARIO_MAX] = {
 	{
 		{
+			{"CYANOGENMOD",		tune_cyanogenmod},
 			{"DYNAMIC_UI",		tune_dynamic_ui},
 			{"DYNAMIC_VIDEO",	tune_dynamic_video},
 			{"DYNAMIC_VIDEO",	tune_dynamic_video},
@@ -742,6 +763,7 @@ static struct mdnie_tunning_info tunning_table[CABC_MAX][MODE_MAX][SCENARIO_MAX]
 			{"DYNAMIC_GALLERY",	tune_dynamic_gallery},
 			{"DYNAMIC_VT",		tune_dynamic_vt},
 		}, {
+			{"CYANOGENMOD",		tune_cyanogenmod},
 			{"STANDARD_UI",		tune_standard_ui},
 			{"STANDARD_VIDEO",	tune_standard_video},
 			{"STANDARD_VIDEO",	tune_standard_video},
@@ -751,6 +773,7 @@ static struct mdnie_tunning_info tunning_table[CABC_MAX][MODE_MAX][SCENARIO_MAX]
 			{"STANDARD_GALLERY",	tune_standard_gallery},
 			{"STANDARD_VT",		tune_standard_vt},
 		}, {
+			{"CYANOGENMOD",		tune_cyanogenmod},
 			{"NATURAL_UI",		tune_natural_ui},
 			{"NATURAL_VIDEO",	tune_natural_video},
 			{"NATURAL_VIDEO_WARM",	tune_natural_video},
@@ -760,6 +783,7 @@ static struct mdnie_tunning_info tunning_table[CABC_MAX][MODE_MAX][SCENARIO_MAX]
 			{"NATURAL_GALLERY",	tune_natural_gallery},
 			{"NATURAL_VT",		tune_natural_vt},
 		}, {
+			{"CYANOGENMOD",		tune_cyanogenmod},
 			{"MOVIE_UI",		tune_movie_ui},
 			{"MOVIE_VIDEO",		tune_movie_video},
 			{"MOVIE_VIDEO",		tune_movie_video},