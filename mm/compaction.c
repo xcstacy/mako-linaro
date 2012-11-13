ca7386f9430e51ac35dbecc72e563ea9c1ef6d69
Raymond Golo (intersectRaven) <rjgolo@gmail.com>
2012-11-13 22:56:52 +0800

    Support asynchronous compaction in lowmemorykiller. (adapted from Steve Kondik's commit from N7 kernel)
    Conflicts:
    
    	drivers/staging/android/lowmemorykiller.c
    
    Signed-off-by: Luis Cruz <ljc2491@gmail.com>

--- mm/compaction.c
+++ mm/compaction.c
@@ -835,18 +835,18 @@ int compact_pgdat(pg_data_t *pgdat, int order)
 	return __compact_pgdat(pgdat, &cc);
 }
 
-static int compact_node(int nid)
+int compact_node(int nid, bool sync)
 {
 	struct compact_control cc = {
 		.order = -1,
-		.sync = true,
+		.sync = sync,
 	};
 
 	return __compact_pgdat(NODE_DATA(nid), &cc);
 }
 
 /* Compact all nodes in the system */
-static void compact_nodes(void)
+void compact_nodes(bool sync)
 {
 	int nid;
 
@@ -854,7 +854,7 @@ static void compact_nodes(void)
 	lru_add_drain_all();
 
 	for_each_online_node(nid)
-		compact_node(nid);
+		compact_node(nid, sync);
 }
 
 /* The written value is actually unused, all memory is compacted */
@@ -865,7 +865,7 @@ int sysctl_compaction_handler(struct ctl_table *table, int write,
 			void __user *buffer, size_t *length, loff_t *ppos)
 {
 	if (write)
-		compact_nodes();
+		compact_nodes(true);
 
 	return 0;
 }
