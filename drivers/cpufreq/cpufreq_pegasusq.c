778864c00e3290374d6ea76594f70fda21aacb8e
Gokhan Moral <gm@alumni.bilkent.edu.tr>
2012-12-05 01:15:20 -0800

    cpufreq_pegasusq: set sampling_rate to 200000 when the screen is off even if we have another sampling rate set

--- drivers/cpufreq/cpufreq_pegasusq.c
+++ drivers/cpufreq/cpufreq_pegasusq.c
@@ -1414,7 +1414,7 @@ static void cpufreq_pegasusq_early_suspend(struct early_suspend *h)
 	prev_freq_step = dbs_tuners_ins.freq_step;
 	prev_sampling_rate = dbs_tuners_ins.sampling_rate;
 	dbs_tuners_ins.freq_step = 10;
-	dbs_tuners_ins.sampling_rate *= 4;
+	dbs_tuners_ins.sampling_rate = 200000;
 #if EARLYSUSPEND_HOTPLUGLOCK
 	atomic_set(&g_hotplug_lock,
 	    (dbs_tuners_ins.min_cpu_lock) ? dbs_tuners_ins.min_cpu_lock : 1);
