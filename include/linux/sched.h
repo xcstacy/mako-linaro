09a9b3ab3087189242a5a57409206c53a87d3c27
Juri Lelli <juri.lelli@gmail.com>
2013-05-15 15:38:17 +0200

    futex: priority boosting, take 2

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1577,8 +1577,8 @@ struct task_struct {
 	/*
 	 * PI-CV handling
 	 *
-	 * cv_waiters: waiters on a condvar this task is helping
-	 *	       to happen
+	 * cv_waiters: waiters for a condition this task is helping
+	 *	       to happen (only the top ones)
 	 * cond_waiter: pointer to the struct with which this
 	 *		task is queued in a condvar hash bucket
 	 */
