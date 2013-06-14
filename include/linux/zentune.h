044508fc93f780a18ccea71d52da57ed8ff5df20
Steven Barrett <damentz@liquorix.net>
2013-06-13 21:25:47 -0500

    zen-tune: change default up_threshold and down_differential to be aggressive
    On new CPU architectures, a high CPU frequency doesn't translate to signficantly
    higher power draw when the CPU is idle.  Lets be more aggressive at scaling up
    to mid and maximum frequencies as early as 30% CPU load.  After 60%, go for
    maximum frequency.  Down sampling factor is still 10, so when above 60%, checks
    happen at a factor of 10 less often decreasing context switching even further.
    
    Conflicts:
    
    	drivers/cpufreq/cpufreq_ondemand.c
    	include/linux/zentune.h
    
    Signed-off-by: Luis Cruz <ljc2491@gmail.com>

--- include/linux/zentune.h
+++ include/linux/zentune.h
@@ -5,22 +5,26 @@
 
 /* CPU Scheduler Related */
 
-#define sysctl_sched_latency_custom 3000000ULL;
-#define normalized_sysctl_sched_latency_custom 3000000ULL;
-#define sysctl_sched_min_granularity_custom 300000ULL;
-#define normalized_sysctl_sched_min_granularity_custom 300000ULL;
-#define sched_nr_latency_custom 10
-#define sysctl_sched_wakeup_granularity_custom 500000UL;
-#define normalized_sysctl_sched_wakeup_granularity_custom 500000UL;
-#define sysctl_sched_migration_cost_custom 250000UL;
-#define sysctl_sched_cfs_bandwidth_slice_custom 3000UL;
+#  define sysctl_sched_latency_custom                       3000000ULL;
+#  define normalized_sysctl_sched_latency_custom            3000000ULL;
+#  define sysctl_sched_min_granularity_custom               300000ULL;
+#  define normalized_sysctl_sched_min_granularity_custom    300000ULL;
+#  define sched_nr_latency_custom                           10;
+#  define sysctl_sched_wakeup_granularity_custom            500000UL;
+#  define normalized_sysctl_sched_wakeup_granularity_custom 500000UL;
+#  define sysctl_sched_migration_cost_custom                250000UL;
+#  define sysctl_sched_cfs_bandwidth_slice_custom           3000UL;
 
 /* MM Related */
 #define vm_dirty_ratio_custom 50;
 #define dirty_background_ratio_custom 20;
 
-/* CPUFREQ Related */
-#define DEF_SAMPLING_DOWN_FACTOR_CUSTOM 10
+/* CPUFreq Related */
+#define DEF_SAMPLING_DOWN_FACTOR_CUSTOM          (10)
+#define DEF_FREQUENCY_DOWN_DIFFERENTIAL_CUSTOM   (30)
+#define DEF_FREQUENCY_UP_THRESHOLD_CUSTOM        (60)
+#define MICRO_FREQUENCY_DOWN_DIFFERENTIAL_CUSTOM (30)
+#define MICRO_FREQUENCY_UP_THRESHOLD_CUSTOM      (60)
 
 #endif /* __KERNEL__ */
 #endif /* _LINUX_ZENTUNE_H */
