21b35470006f1d0389bc028976ce5c1aa74efefa
Francisco Franco <franciscofranco.1990@gmail.com>
2013-06-20 18:29:44 +0100

    msm: rq_stats: in my opinion iowait should not be counted as idle time, so subtract iowait from idle on the load calculations.
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- arch/arm/mach-msm/msm_rq_stats.c
+++ arch/arm/mach-msm/msm_rq_stats.c
@@ -56,42 +56,31 @@ struct cpu_load_data {
 
 static DEFINE_PER_CPU(struct cpu_load_data, cpuload);
 
-static inline u64 get_cpu_idle_time_jiffy(unsigned int cpu, u64 *wall)
+static inline cputime64_t get_cpu_idle_time_jiffy(unsigned int cpu,
+              cputime64_t *wall)
 {
-	u64 idle_time;
-	u64 cur_wall_time;
-	u64 busy_time;
+  	u64 idle_time;
+  	u64 cur_wall_time;
+  	u64 busy_time;
 
-	cur_wall_time = jiffies64_to_cputime64(get_jiffies_64());
+  	cur_wall_time = jiffies64_to_cputime64(get_jiffies_64());
 
-	busy_time  = kcpustat_cpu(cpu).cpustat[CPUTIME_USER];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SYSTEM];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_IRQ];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SOFTIRQ];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_STEAL];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_NICE];
+  	busy_time  = kcpustat_cpu(cpu).cpustat[CPUTIME_USER];
+  	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SYSTEM];
+  	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_IRQ];
+  	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SOFTIRQ];
+  	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_STEAL];
+  	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_NICE];
 
-	idle_time = cur_wall_time - busy_time;
-	if (wall)
-		*wall = jiffies_to_usecs(cur_wall_time);
+  	idle_time = cur_wall_time - busy_time;
+  	
+  	if (wall)
+    	*wall = jiffies_to_usecs(cur_wall_time);
 
-	return jiffies_to_usecs(idle_time);
+  	return jiffies_to_usecs(idle_time);
 }
 
-static inline cputime64_t get_cpu_idle_time(unsigned int cpu, cputime64_t *wall)
-{
-	u64 idle_time = get_cpu_idle_time_us(cpu, NULL);
-
-	if (idle_time == -1ULL)
-		return get_cpu_idle_time_jiffy(cpu, wall);
-	else
-		idle_time += get_cpu_iowait_time_us(cpu, wall);
-
-	return idle_time;
-}
-
-static inline cputime64_t get_cpu_iowait_time(unsigned int cpu,
-							cputime64_t *wall)
+static inline cputime64_t get_cpu_iowait_time(unsigned int cpu, cputime64_t *wall)
 {
 	u64 iowait_time = get_cpu_iowait_time_us(cpu, wall);
 
@@ -99,6 +88,18 @@ static inline cputime64_t get_cpu_iowait_time(unsigned int cpu,
 		return 0;
 
 	return iowait_time;
+}
+
+static inline cputime64_t get_cpu_idle_time(unsigned int cpu,
+              cputime64_t *wall)
+{
+  	u64 idle_time = get_cpu_idle_time_us(cpu, wall) - 
+  						get_cpu_iowait_time(cpu, wall);
+
+  	if (idle_time == -1ULL)
+    	idle_time = get_cpu_idle_time_jiffy(cpu, wall);
+
+  	return idle_time;
 }
 
 static int update_average_load(unsigned int freq, unsigned int cpu)
