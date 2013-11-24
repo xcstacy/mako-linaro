762e47c774ec843f82f479892636174e81a730b8
Matt Wagantall <mattw@codeaurora.org>
2013-11-24 18:15:42 -0500

    ondemand gov: fixup
    cpufreq: ondemand: Fix hotplug deadlock with store_powersave_bias
    
    store_powersave_bias() acquires the hotplug lock and the dbs_mutex
    lock, but does so in the wrong order.  Deadlocks like the following
    can result.
    
    Thread A:
        get_online_cpus+0x3c/0x5c           <- acquires 'cpu_hotplug.lock'
        store_powersave_bias+0x80/0x3f4     <- acquires 'dbs_mutex'
        kobj_attr_store+0x14/0x20
        sysfs_write_file+0x108/0x13c
        vfs_write+0xb0/0x128
        sys_write+0x38/0x64
    
    Thread B:
        cpufreq_governor_dbs+0x7c/0x55c     <- acquires 'dbs_mutex'
        __cpufreq_governor+0x90/0xe0
        __cpufreq_set_policy+0x1b0/0x258
        cpufreq_add_dev_interface+0x2cc/0x334
        cpufreq_add_dev+0x514/0x580
        cpufreq_cpu_callback+0x88/0x9c
        notifier_call_chain+0x38/0x68
        __cpu_notify+0x28/0x40
        _cpu_up+0xe4/0x118                  <- acquires 'cpu_hotplug.lock'
        cpu_up+0x64/0x80
        store_online+0x48/0x78
        dev_attr_store+0x18/0x24
        sysfs_write_file+0x108/0x13c
        vfs_write+0xb0/0x128
        sys_write+0x38/0x64
    
    Fix this by flipping the order in which the locks are acquired and
    released in store_powersave_bias so that it is the same as in the
    hotplug path.
    
    Change-Id: Idc59fb29d60b8f7fceb8ed0f2bb9eff4670abda7
    Signed-off-by: Matt Wagantall <mattw@codeaurora.org>
    cpufreq: ondemand: Fix locking issue in store_powersave_bias
    
    store_powersave_bias takes timer_mutex before calling dbs_timer_exit
    which tries to cancel the delayed work do_dbs_timer which in turn
    tries to take the same lock. This can cause a lock recursion under
    the race condition where can cancel_delayed_work_sync is called
    when the work has already started executing.
    
    This can be avoided by taking that lock after calling
    dbs_timer_exit.
    
    Change-Id: I7f862286e66f1ddc1e13e4eeee369dd188fc10d5
    Signed-off-by: Rohit Gupta <rohgup@codeaurora.org>
    cpufreq: ondemand: Fix locking in store_powersave_bias
    
    store_powersave_bias() assumes that dbs_info->cur_policy will
    not change while it is executing. If it does, memory corruption
    can potentially result. Fix this by acquiring the dbs_lock.
    
    Most noticeably, this resolves timer-related list corruptions
    encountered when queue_delayed_work_on() is called from within
    store_powersave_bias() while a CPU is being hotplugged in and
    out.
    
    CRs-Fixed: 494407
    Change-Id: I0fd5cbef6834545274cad9481e574ec50b37b871
    Signed-off-by: Matt Wagantall <mattw@codeaurora.org>
    cpufreq: ondemand: Fix store_powersave_bias() race with hotplug
    
    store_powersave_bias() assumes that CPUs will not be taken offline or
    brought online while executing its for_each_online_cpu() loops. Fix
    this race by blocking hotplug while executing in these loop.
    
    CRs-Fixed: 494407
    Change-Id: I0d5520444ec21a8d9b12fb73c06ac8442449fa85
    Signed-off-by: Matt Wagantall <mattw@codeaurora.org>
    cpufreq: ondemand: Fix update_sampling_rate race with hotplug
    
    update_sampling_rate has a for loop which goes through each
    online cpu and possibly queue up the ondemand work for them.
    But while doing this it doesnt take any hotplug lock which
    could potentially cause a race condition where ondemand work
    is queued after the hotplug code (which sets the policy to NULL)
    in the governor has cancelled any pending work. This could cause
    a crash while trying to access the NULL policy in dbs_check_cpu.
    
    Protecting the for_each_online_cpu loop with get_online_cpus()
    and put_online_cpus().
    
    Change-Id: Ia3f43ca7e4bed542834ab03ca1191d728f13311c
    Signed-off-by: Rohit Gupta <rohgup@codeaurora.org>

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -112,7 +112,7 @@ static inline void dbs_timer_exit(struct cpu_dbs_info_s *dbs_info);
 static unsigned int dbs_enable;	/* number of CPUs using this policy */
 
 /*
- * dbs_mutex protects dbs_enable in governor start/stop.
+ * dbs_mutex protects dbs_enable and dbs_info during start/stop.
  */
 static DEFINE_MUTEX(dbs_mutex);
 
@@ -343,6 +343,7 @@ static void update_sampling_rate(unsigned int new_rate)
 	dbs_tuners_ins.sampling_rate = new_rate
 				     = max(new_rate, min_sampling_rate);
 
+	get_online_cpus();
 	for_each_online_cpu(cpu) {
 		struct cpufreq_policy *policy;
 		struct cpu_dbs_info_s *dbs_info;
@@ -377,6 +378,7 @@ static void update_sampling_rate(unsigned int new_rate)
 		}
 		mutex_unlock(&dbs_info->timer_mutex);
 	}
+	put_online_cpus();
 }
 
 static ssize_t store_sampling_rate(struct kobject *a, struct attribute *b,
@@ -580,6 +582,10 @@ static ssize_t store_powersave_bias(struct kobject *a, struct attribute *b,
 				POWERSAVE_BIAS_MINLEVEL));
 
 	dbs_tuners_ins.powersave_bias = input;
+
+	get_online_cpus();
+	mutex_lock(&dbs_mutex);
+
 	if (!bypass) {
 		if (reenable_timer) {
 			/* reinstate dbs timer */
@@ -632,20 +638,23 @@ skip_this_cpu:
 
 			if (dbs_info->cur_policy) {
 				/* cpu using ondemand, cancel dbs timer */
-				mutex_lock(&dbs_info->timer_mutex);
 				dbs_timer_exit(dbs_info);
 
+				mutex_lock(&dbs_info->timer_mutex);
 				ondemand_powersave_bias_setspeed(
 					dbs_info->cur_policy,
 					NULL,
 					input);
-
 				mutex_unlock(&dbs_info->timer_mutex);
+
 			}
 skip_this_cpu_bypass:
 			unlock_policy_rwsem_write(cpu);
 		}
 	}
+
+	mutex_unlock(&dbs_mutex);
+	put_online_cpus();
 
 	return count;
 }
