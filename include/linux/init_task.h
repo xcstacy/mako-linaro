76ae7c06f10407bc777e7e9241cb2c070de7447f
xcstacy <zkchong2005@hotmail.com>
2013-11-29 13:22:31 +0000

    SaberMod r195, -ofast and build fix

--- include/linux/init_task.h
+++ include/linux/init_task.h
@@ -161,7 +161,6 @@ extern struct cred init_cred;
 	},								\
 	.tasks		= LIST_HEAD_INIT(tsk.tasks),			\
 	INIT_PUSHABLE_TASKS(tsk)					\
-	INIT_CGROUP_SCHED(tsk)						\
 	.proxying_for	= NULL,						\
 	.proxied_by	= NULL,						\
 	.proxies	= LIST_HEAD_INIT(tsk.proxies),			\
